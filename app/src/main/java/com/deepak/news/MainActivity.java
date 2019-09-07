1e2ca4faa4afcc002ef16a45719a0f0cf43d091d
deepak kumar <deepak24121994@gmail.com>
2019-09-07 14:05:08 +0530

    Designing  the UI

--- app/src/main/java/com/deepak/news/MainActivity.java
+++ app/src/main/java/com/deepak/news/MainActivity.java
@@ -1,14 +1,29 @@
 package com.deepak.news;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    //ui
+    private EditText searchField;
+    private ImageButton searchButton;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
+
+    private void init() {
+        searchField=findViewById(R.id.search_field);
+        searchButton=findViewById(R.id.search_button);
+        recyclerView=findViewById(R.id.result_list);
+    }
+
 }
